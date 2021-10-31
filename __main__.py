5af2b46bc527686c77e2019ba85433f438786b96
Chris Diehl <cultclassik@gmail.com>
2021-10-31 08:10:22 -0500

    add service

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
 from pulumi_kubernetes.core.v1 import ContainerArgs, ContainerPortArgs, PodSpecArgs, PodTemplateSpecArgs
 from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
+from pulumi_kubernetes.apps.v1 import Service, ServiceSpecArgs
 
 config = pulumi.Config()
 
@@ -30,3 +31,22 @@ def nginx(config):
 
 nginxDeployment = nginx(config)
 pulumi.export("nginx", nginxDeployment.metadata.apply(lambda m: m.name))
+
+def nginx_svc(config):
+    nginxLabels = {"app": "nginx"}
+    nginxService = Service(
+        "nginx-svc",
+        spec=ServiceSpecArgs(
+            type="ClusterIP",
+            selector=LabelSelectorArgs(match_labels=nginxLabels),
+            ports=[ServicePortsSpecArgs(
+                name="80-80",
+                protocol="TCP",
+                port="80",
+                targetPort="80",
+            )],
+        ))
+    return nginxService
+     
+nginxService = nginx_svc(config)
+pulumi.export("nginx_svc", nginxDeployment.metadata.apply(lambda m: m.name))
